767ec0bd1b5c9de7430541d094ae00daabbe33e1
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2022-08-15 20:55:06 -0700

    Create index and show actions

--- api_challenge/api_tree/app/controllers/api/trees_controller.rb
+++ api_challenge/api_tree/app/controllers/api/trees_controller.rb
@@ -1,9 +1,14 @@
 class Api::TreesController < ApplicationController
 
      # GET all Trees
-     def index
+    def index
         @trees = Tree.all
         render json: @trees
     end
-    
+
+    # GET full Tree
+    def show
+        @tree = Tree.find(params[:id])
+        render json: @tree
+    end 
 end
