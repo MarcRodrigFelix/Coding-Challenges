4587f64c377989da55d5640f66c8c2e8fa1d7b15
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2022-08-15 22:48:20 -0700

    Add active model serializer to gem

--- api_challenge/api_tree/app/controllers/api/trees_controller.rb
+++ api_challenge/api_tree/app/controllers/api/trees_controller.rb
@@ -3,12 +3,13 @@ class Api::TreesController < ApplicationController
      # GET all Trees
     def index
         @trees = Tree.all
-        render json: @trees
+        render json: @trees.to_json( only: [ :id, :label ], include: [ :children ] )
     end
 
     # GET full (:id) Tree
     def show
         @tree = Tree.find(params[:id])
+        @children = @tree.children
         render json: @tree
     end 
 end
