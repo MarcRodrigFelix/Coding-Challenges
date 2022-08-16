6b0f4d7a79970658027427a37fc0b5cd1b6b8975
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2022-08-16 00:09:37 -0700

    Add correct routes for api/tree GET and POST

--- api_challenge/api_tree/app/serializers/tree_serializer.rb
+++ api_challenge/api_tree/app/serializers/tree_serializer.rb
@@ -1,6 +1,5 @@
 class TreeSerializer < ActiveModel::Serializer
   attributes :id, :label, :children
   has_many :children
-  # has_one :children
 
 end
--- api_challenge/api_tree/config/routes.rb
+++ api_challenge/api_tree/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
-    resources :trees
-  end
+  # namespace :api do
+  #   resources :trees
+  # end
+
+  get "api/tree", to: "api/trees#index"
+  get "api/tree/:id", to: "api/trees#show"
+  post "api/tree", to: "api/trees#create"
 end
