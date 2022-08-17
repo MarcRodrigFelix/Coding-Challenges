9107bc3f8d62be77c659bcafe37b532d65463d81
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2022-08-16 21:13:39 -0700

    Create method to substitute spaces with zeros

--- minesweeper/minesweeper.rb
+++ minesweeper/minesweeper.rb
@@ -3,8 +3,34 @@ require 'pry'
 class Board
 
     def self.transform input
+        # remove first row and last ("|------|")
+            # substitute spaces for ints
+                # pass into check cells method that will iterate over each row, index, and current object
+        check_cells(subzero(input[1..-2]))
+    end
+
+    private 
+
+    class << self
+
+        def check_cells cells
+            # iterate over object, each row and index
+            cells.each.with_index.with_object(cells) do |(row, row_ind), obj|
+                # row.each.with_index do |cell, cell_ind|
+                #     puts cell, cell_ind
+                # end
 binding.pry
-        puts input
+            end
+        end
+
+
+        def subzero input
+            input.map do |row|
+                # for each row, substitute spaces for zeros
+                row.gsub(/[^*\s]/, '').chars.map{ |el| el == '*' ? nil : 0 }
+            end
+        end
+        
     end
 
 end
